6185c6abb6c33589ed211054feb24bcba371c91e
Johannes Arnold <johannes.arnold@stud.uni-hannover.de>
2023-02-22 11:14:00 +0100

    Read onboard temperature sensor

--- main.py
+++ main.py
@@ -2,25 +2,37 @@ import json
 import errno
 
 from lib.dht import DHT22
-from machine import Pin
+from machine import Pin, ADC
+
 
 led = Pin("LED", Pin.OUT)
 sensor = DHT22(Pin(15, Pin.IN, Pin.PULL_UP))
+
+sensor_temp = ADC(4)
+conversion_factor = 3.3 / (1 << 16)
+
+
+def measure_onboard() -> float:
+    voltage = sensor_temp.read_u16() * conversion_factor
+    c = 27 - (voltage - 0.706) / 0.001721
+    return round(c, 1)
 
 
 def measure() -> dict:
     t = 0
     h = 0
+    o = 0
     err = 0
 
     try:
         sensor.measure()
         t = sensor.temperature()
         h = sensor.humidity()
+        o = measure_onboard()
     except OSError as exc:
         err = exc.errno
 
-    return {"temperature": t, "humidity": h, "error": err}
+    return {"temperature": t, "onboard": o, "humidity": h, "error": err}
 
 
 def report(t):
